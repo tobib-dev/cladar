c06646674c8cb5856407ffb355625c620c5def34
tobii19-py <tobi19.py@gmail.com>
2025-06-21 11:28:42 -0500

    Updated departments table to prevent circular dependency

--- sql/schema/004_departments.sql
+++ sql/schema/004_departments.sql
@@ -1,8 +1,7 @@
 -- +goose Up
 CREATE TABLE departments (
     id UUID PRIMARY KEY,
-    dept_name TEXT NOT NULL UNIQUE,
-    manager_id UUID NOT NULL REFERENCES managers(id) ON DELETE CASCADE
+    dept_name TEXT NOT NULL UNIQUE
 );
 
 -- +goose Down
