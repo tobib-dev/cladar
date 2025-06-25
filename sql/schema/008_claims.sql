4a01f7ff459821fcafa68d7f9b02ed09f2577546
tobi <tobi19.py@gmail.com>
2025-06-25 14:53:28 -0500

    Create table claims

---
+++ sql/schema/008_claims.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+CREATE type STATUS AS ENUM ('declined', 'awarded', 'completed')
+
+CREATE TABLE claims(
+    id UUID PRIMARY KEY,
+    customer_id UUID NOT NULL references customers(id) ON DELETE CASCADE,
+    agent_id UUID NOT NULL references agents(id) ON DELETE CASCADE,
+    claim_type TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    current_status STATUS NOT NULL,
+    award REAL,
+);
+
+-- +goose Down
+DROP TYPE STATUS;
+DROP TABLE claims;
